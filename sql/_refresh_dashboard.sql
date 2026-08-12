455d55bbde25e950bde78aab681207e5e5f4a444
eliannachr98-cell <eliannachr.98@gmail.com>
2026-08-12 15:47:00 +0300

    diag: time the refresh

--- sql/_refresh_dashboard.sql
+++ sql/_refresh_dashboard.sql
@@ -1,1 +1,3 @@
+select clock_timestamp() as started;
 select public.refresh_dashboard_caches();
+select clock_timestamp() as finished, payload->'total' as total, refreshed_at from public.dashboard_cache;
