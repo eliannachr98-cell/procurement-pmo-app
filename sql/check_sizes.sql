187041bc7fb87cdd8ca8c2c4c740a4dc671d31c2
eliannachr98-cell <eliannachr.98@gmail.com>
2026-08-19 12:56:09 +0300

    Check indexes on the joined tables

--- sql/check_sizes.sql
+++ sql/check_sizes.sql
@@ -1,2 +1,4 @@
-set statement_timeout = '90000';
-select public.dashboard_breakdown(p_contract_type := array['Έργα']) is not null as ok;
+select indexname, indexdef
+from pg_indexes
+where schemaname = 'public' and tablename in ('record_cpvs_compact', 'record_contractors_compact', 'awards_compact', 'contracts_compact')
+order by tablename, indexname;
