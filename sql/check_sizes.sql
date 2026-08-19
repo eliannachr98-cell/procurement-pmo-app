fc7b8f600b5d2e7d1fcda0d47f7ce2440ae99ce7
eliannachr98-cell <eliannachr.98@gmail.com>
2026-08-19 20:14:58 +0300

    Diagnostic: check PostgREST role statement_timeout config

--- sql/check_sizes.sql
+++ sql/check_sizes.sql
@@ -1,4 +1,5 @@
-set statement_timeout = '60000';
-select clock_timestamp() as t_start;
-select public.dashboard_breakdown(p_contract_type := array['Προμήθειες']) is not null as ok;
-select clock_timestamp() as t_end;
+select rolname, rolconfig
+from pg_roles
+where rolname in ('authenticator','anon','authenticated','postgres','service_role');
+
+show statement_timeout;
