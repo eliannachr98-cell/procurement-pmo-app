af348192d49c9cb96220c44db55efc1d9d74a25f
eliannachr98-cell <eliannachr.98@gmail.com>
2026-08-21 09:52:53 +0300

    Diagnostic: check for trigram index and extension on authority_name

--- sql/check_sizes.sql
+++ sql/check_sizes.sql
@@ -1,8 +1,9 @@
-select a.adam, a.procurement_adam, a.title as award_title, p.title as notice_title
-from public.awards_compact a
-join public.record_contractors_compact c
-  on c.record_type = 'award' and c.record_adam = a.adam
-left join public.procurements_compact p
-  on p.adam = a.procurement_adam
-where c.contractor_vat = '094007885'
-order by a.adam;
+select extname from pg_extension where extname = 'pg_trgm';
+
+select indexname, indexdef
+from pg_indexes
+where tablename = 'procurements_compact'
+  and indexdef ilike '%authority_name%';
+
+select count(distinct authority_name) as distinct_authorities
+from public.procurements_compact;
