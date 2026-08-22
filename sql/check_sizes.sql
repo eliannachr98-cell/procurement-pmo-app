f6bca604c2822e7cfeb7a3c1df5595980c667f44
eliannachr98-cell <eliannachr.98@gmail.com>
2026-08-23 02:38:34 +0300

    Reset demo recipient notification history to re-preview with publication date

--- sql/check_sizes.sql
+++ sql/check_sizes.sql
@@ -1,8 +1,2 @@
 delete from public.alert_notifications_sent
 where recipient_email = 'eliannachr.98@gmail.com';
-
-select public.alert_email_candidates(
-  'eliannachr.98@gmail.com',
-  (select array_agg(cpv_code) from public.cpv_watchlist),
-  (select array_agg(nuts_code) from public.alert_nuts_filter)
-) as candidates;
