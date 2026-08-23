9c3941071b18e8ad4742760b2e15a8e321bd8073
eliannachr98-cell <eliannachr.98@gmail.com>
2026-08-23 12:03:30 +0300

    diag: check record_type values in record_contractors_compact

--- sql/check_sizes.sql
+++ sql/check_sizes.sql
@@ -1,4 +1,1 @@
-select column_name, data_type
-from information_schema.columns
-where table_name = 'record_contractors_compact'
-order by column_name;
+select record_type, count(*) from public.record_contractors_compact group by record_type;
