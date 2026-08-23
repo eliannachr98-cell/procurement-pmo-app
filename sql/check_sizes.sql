910aa73f9c7f0b5921de499ea34ae4cb80578a1b
eliannachr98-cell <eliannachr.98@gmail.com>
2026-08-23 15:25:07 +0300

    diag: check why welcomeEmailSent was false for ilianna.charisi

--- sql/check_sizes.sql
+++ sql/check_sizes.sql
@@ -1,1 +1,7 @@
-select email, created_at from public.alert_recipients order by created_at desc;
+select count(*) as watchlist_count from public.cpv_watchlist;
+select public.alert_email_candidates(
+  'ilianna.charisi@pwc.com',
+  (select array_agg(cpv_code) from public.cpv_watchlist),
+  null,
+  365
+);
