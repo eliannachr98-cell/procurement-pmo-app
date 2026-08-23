eb7eda0bb936271ea35e13731e9049c24c79d0fa
eliannachr98-cell <eliannachr.98@gmail.com>
2026-08-23 12:36:18 +0300

    diag: check for tracked tenders that already have an award

--- sql/check_sizes.sql
+++ sql/check_sizes.sql
@@ -1,1 +1,9 @@
-select record_type, count(*) from public.record_contractors_compact group by record_type;
+select s.adam as tender_adam, 'submitted' as kind, a.adam as award_adam, a.award_date, a.amount_inc_vat
+from public.alert_submissions s
+join public.awards_compact a on a.procurement_adam = s.adam
+where a.cancelled_at is null
+union all
+select i.adam, 'interested', a.adam, a.award_date, a.amount_inc_vat
+from public.alert_interests i
+join public.awards_compact a on a.procurement_adam = i.adam
+where a.cancelled_at is null;
