e595e5d03265fb0006def4ce49dfccf4f84558f1
eliannachr98-cell <eliannachr.98@gmail.com>
2026-08-23 12:41:00 +0300

    diag: find a sane recent award to test with

--- sql/check_sizes.sql
+++ sql/check_sizes.sql
@@ -1,8 +1,7 @@
-select a.adam, a.procurement_adam, a.award_date, a.amount_inc_vat,
-  (select string_agg(rc.contractor_name, ', ' order by rc.position)
-   from public.record_contractors_compact rc
-   where rc.record_adam = a.adam and rc.record_type = 'award') as contractors
+select a.procurement_adam, a.adam as award_adam, a.award_date, a.amount_inc_vat
 from public.awards_compact a
 where a.cancelled_at is null
-order by a.award_date desc nulls last
-limit 5;
+  and a.procurement_adam is not null
+  and a.award_date between '2026-01-01' and '2026-12-31'
+order by a.award_date desc
+limit 3;
